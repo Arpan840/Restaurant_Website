f5ea6e6a0a018170e8b4c53ce45ed649a3a2069f
Arpan Das <arpandas020498@gmail.com>
2023-07-30 17:09:31 +0530

    filters added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import data from './data.json' assert {type: 'json'};
 
 let search= document.getElementById('search')
+let vegFilter = document.getElementsByClassName('filter-2')[0]
+let nonVegFilter = document.getElementsByClassName('filter-3')[0]
+let allFood =document.querySelector('.filter-1')
 let foodArr= [];
 function foodData(){
    let foodItems= ''
@@ -32,6 +35,8 @@ addEventListener('input', (e) => {
       
       return i.name.toLowerCase().includes(val) || i.time.toLowerCase().includes(val);
     });
+
+  
   
     let foodItems = '';
     filteredFoodItems.map((i, index) => {
@@ -49,3 +54,64 @@ addEventListener('input', (e) => {
   
     document.querySelector('.food-items').innerHTML = foodItems;
   });
+
+  function filterVeg(){
+    let filterVegFood = foodArr.filter((i)=>{
+     return i.type==="veg";
+    })
+   
+
+   let vegFood = '';
+   filterVegFood.map((i)=>{
+    vegFood+=`
+    <div class="card" >
+    <img src=${i.imageSrc} class="card-img-top dishes" alt="food">
+    <div class="card-body">
+      <h5 class="card-title foodname">${i.name}</h5>
+      <p class="card-text">${i.time}</p>
+    </div>
+  </div>
+    `;
+   })
+   document.querySelector('.food-items').innerHTML = vegFood;
+  }
+   
+  vegFilter.addEventListener('click', filterVeg)
+
+  function filterNonVeg(){
+    let nonVeg = foodArr.filter((i)=>{
+      return i.type === 'non-veg';
+    })
+    let nonVegItems = '';
+   nonVeg.map((i)=>{
+    nonVegItems+=`
+    <div class="card" >
+    <img src=${i.imageSrc} class="card-img-top dishes" alt="food">
+    <div class="card-body">
+      <h5 class="card-title foodname">${i.name}</h5>
+      <p class="card-text">${i.time}</p>
+    </div>
+  </div>
+
+    `
+   })
+   document.querySelector('.food-items').innerHTML = nonVegItems;
+  }
+  nonVegFilter.addEventListener('click', filterNonVeg)
+
+  let allFoodItems=()=>{
+    let allFoods = '';
+    data.map((i)=>{
+      allFoods+=` <div class="card" >
+      <img src=${i.imageSrc} class="card-img-top dishes" alt="food">
+      <div class="card-body">
+        <h5 class="card-title foodname">${i.name}</h5>
+        <p class="card-text">${i.time}</p>
+      </div>
+    </div>`
+    })
+    document.querySelector('.food-items').innerHTML = allFoods;
+   
+  }
+
+  allFood.addEventListener('click', allFoodItems);
